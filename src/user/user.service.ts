5743a9ebac1dfe46e6f20833fff21cbc3ed1ee0c
sewakgautam <sewak.gautam58@gmail.com>
2023-01-31 14:49:53 +0545

    🐞 fix: Exception handeling

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -14,16 +18,21 @@ export class UserService {
   async create(createUserDto: CreateUserDto) {
     const { userId } = createUserDto;
     const checkUser = await this.userRepository.findOne({ where: { userId } });
-    if (checkUser) throw new Error('user Already Exist');
+    if (checkUser) throw new BadRequestException('user Already Exist');
     const createUser = await this.userRepository.save(createUserDto);
     return createUser;
   }
 
   async update(userId: string, updateUserDto: UpdateUserDto) {
+    const checkUser = await this.userRepository.findOne({ where: { userId } });
+    if (!checkUser) throw new NotFoundException('user Not Found');
+
     return await this.userRepository.update({ userId }, { ...updateUserDto });
   }
 
   async remove(userId: string) {
+    const checkUser = await this.userRepository.findOne({ where: { userId } });
+    if (!checkUser) throw new NotFoundException('user Not Found');
     return await this.userRepository.delete({ userId });
   }
 }
