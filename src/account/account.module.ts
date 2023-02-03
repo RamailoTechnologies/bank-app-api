aa3ace5cd3cd1bcae53107e8f8b4281ba7c4e266
sewakgautam <sewak.gautam58@gmail.com>
2023-02-03 15:44:57 +0545

    injecting entity

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -3,9 +3,12 @@ import { AccountService } from './account.service';
 import { AccountController } from './account.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from './entities/account.entity';
+import { Bank } from 'src/bank/entities/bank.entity';
+import { BankBranch } from 'src/bank/entities/bankbranch.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Account])],
+  imports: [TypeOrmModule.forFeature([Account, Bank, BankBranch, User])],
   controllers: [AccountController],
   providers: [AccountService],
 })
