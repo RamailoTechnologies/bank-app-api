5049c9dd63c5407c61f2365a1bb2c5bcb4a76bd0
sewakgautam <sewak.gautam58@gmail.com>
2023-02-13 13:08:32 +0545

    🦄 refactor(validation and swagger):
    changes the swagger date formate

--- src/offers/dto/create-offer.dto.ts
+++ src/offers/dto/create-offer.dto.ts
@@ -1,15 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsDateString, IsEnum, IsString } from 'class-validator';
 import { loanCategory } from '../entities/offer.entity';
 
 export class CreateOfferDto {
+  @IsString()
   @ApiProperty({ type: 'string' })
   offerTitle: string;
+  @IsString()
   @ApiProperty({ type: 'string' })
   offerDescription: string;
+  @IsString()
   @ApiProperty({ type: 'string' })
   offerLink: string;
-  @ApiProperty({ type: 'date' })
+  @IsDateString()
+  @ApiProperty({
+    type: 'string',
+    format: 'date-time',
+    example: '2017-07-21T17:32:28Z',
+  })
   offerValidTill: Date;
+  @IsEnum(loanCategory)
   @ApiProperty({ type: 'string', enum: loanCategory })
   category: loanCategory;
 }
