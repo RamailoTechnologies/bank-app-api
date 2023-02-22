8d102847cd36a4b736f524d10f062a332d0ff198
sewakgautam <sewak.gautam58@gmail.com>
2023-02-22 22:28:32 +0545

    🎈 perf(branch): Delete Branch
    deleted banl branch

--- src/bank/dto/update-bankBranch.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateBankBranchDto } from './create-bankbranch.dto';
-
-export class UpdateBankBranch extends PartialType(CreateBankBranchDto) {}
